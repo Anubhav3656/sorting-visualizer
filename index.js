6c18a43a962312fe9e23b43c7b3e26ee409b43c8
Anubhav Chaudhary <chaudharyanubhav0108@gmail.com>
2023-11-05 00:50:27 +0530

    updating

--- index.js
+++ index.js
@@ -1,76 +1,100 @@
-const container = document.querySelector(".data-container"); 
-function generatebars(num = 10) { 
+let n=prompt("enter no of elements");
+const array=[];
+init();
+function init()
+{
+   for(let i=0;i<n;i++)
+  {
+  array[i]=Math.random();
+  }
+  showBars();
+}
+function play(){
+  const copy=[...array];
+  const moves=bubbleSort(copy);
+ animate(moves);
+}
+function play1(){
+  const copy=[...array];
+  const moves=selectionSort(copy);
+ animate(moves);
+}
+function animate(moves)
+{
+  if(moves.length==0)
+  {
+    showBars();
+    return;
+  }
+  const move=moves.shift();
+  const [i,j]=move.indices;
+  if (move.type=="swap")
+  {
+  [array[i],array[j]]=[array[j],array[i]];
+  }
+  showBars(move);
+  setTimeout(function(){
+    animate(moves);
+  },200);
+}
+function bubbleSort(array)
+{
+  const moves=[];
+ do{
+    var swapped=false;
+    for(let i=1;i<array.length;i++)
+    {
+      moves.push({indices:[i-1,i],type:"comp"});
+      if(array[i-1]>array[i])
+      {
+      swapped=true;
+      moves.push({indices:[i-1,i],type:"swap"});
+      [array[i-1],array[i]]=[array[i],array[i-1]];
+      }
+    }
+  }while(swapped);
+  return moves;
+}
+function selectionSort(array)
+{
+  const moves=[];
+  for(let i=0;i<array.length;i++)
+  {
+    var swapped=false;
+    let min=i;
+    for(let j=i+1;j<array.length;j++)
+    {
+      if(array[j]<array[min])
+      {
+        moves.push({indices:[min,i],type:"comp"});
+        min=j;
+      }
+    }
+    if (min!=i)
+    {
+      swapped=true;
+      moves.push({indices:[min,i],type:"swap"});
+    [array[i],array[min]]=[array[min],array[i]];
+    }
     
-  for (let i = 0; i < num; i += 1) { 
-    const value = Math.floor(Math.random() * 100) + 1;  
-    const bar = document.createElement("div");  
-    bar.classList.add("bar"); 
-    bar.style.height = `${value * 3}px`; 
-    bar.style.transform = `translateX(${i * 30}px)`; 
-    const barLabel = document.createElement("label"); 
-    barLabel.classList.add("bar_id"); 
-    barLabel.innerHTML = value; 
-    bar.appendChild(barLabel); 
-    container.appendChild(bar); 
-  } 
-} 
-  
+  }
+  return moves;
+}
 
-async function SelectionSort(delay = 300) { 
-  let bars = document.querySelectorAll(".bar"); 
-   var min_idx = 0; 
-   for (var i = 0; i < bars.length; i += 1) { 
-    min_idx = i; 
-    bars[i].style.backgroundColor = "darkblue"; 
-    for (var j = i + 1; j < bars.length; j += 1) { 
-      bars[j].style.backgroundColor = "red"; 
-      await new Promise((resolve) => setTimeout(() => { resolve(); }, 300) ); 
-      var val1 = parseInt(bars[j].childNodes[0].innerHTML);
-      var val2 = parseInt(bars[min_idx].childNodes[0].innerHTML); 
-      if (val1 < val2) { 
-        if (min_idx !== i) { 
-          bars[min_idx].style.backgroundColor = "  rgb(24, 190, 255)"; 
-        } 
-        min_idx = j; 
-      } else { 
-        bars[j].style.backgroundColor = "  rgb(24, 190, 255)"; 
-      } 
-    } 
-   
-    var temp1 = bars[min_idx].style.height; 
-    var temp2 = bars[min_idx].childNodes[0].innerText; 
-    bars[min_idx].style.height = bars[i].style.height; 
-    bars[i].style.height = temp1; 
-    bars[min_idx].childNodes[0].innerText = bars[i].childNodes[0].innerText; 
-    bars[i].childNodes[0].innerText = temp2; 
-      
-    await new Promise((resolve) => 
-      setTimeout(() => { 
-        resolve(); 
-      }, 300) 
-    ); 
-    bars[min_idx].style.backgroundColor = "  rgb(24, 190, 255)"; 
-    bars[i].style.backgroundColor = " rgb(49, 226, 13)"; 
-  } 
-  document.getElementById("Button1").disabled = false; 
-  document.getElementById("Button1").style.backgroundColor = "#6f459e"; 
-  document.getElementById("Button2").disabled = false; 
-  document.getElementById("Button2").style.backgroundColor = "#6f459e"; 
-} 
-generatebars(); 
-  
- function generate() 
-{ 
-  window.location.reload(); 
- } 
- 
- 
-function disable() 
-{ 
-  document.getElementById("Button1").disabled = true; 
-  document.getElementById("Button1").style.backgroundColor = "#d8b6ff"; 
-  document.getElementById("Button2").disabled = true; 
-  document.getElementById("Button2").style.backgroundColor = "#d8b6ff";   
-  document.getElementById("Button3").disabled = true; 
-  document.getElementById("Button3").style.backgroundColor = "#d8b6ff";   
+function showBars(move)
+{
+  container.innerHTML="";
+    for(let i=0;i<array.length;i++)
+    {
+       const bar=document.createElement("div");
+       bar.style.height=array[i]*200+"%";
+       
+       bar.classList.add("bar");
+       if(move && move.indices.includes(i))
+       {
+         bar.style.backgroundColor= 
+              move.type=="swap"?"red":"blue";
+       }
+         container.appendChild(bar);
+    }
 }
